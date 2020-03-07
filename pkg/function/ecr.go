1cf1c583e21088148f9f7544e710f3c2d8734f97
Simon Woldemichael <swoldemichael@acm.org>
2020-03-06 21:34:28 -0600

    Encapsulate CheckRepositoryCompliance under Container
    Complete BatchCheckRepositoryCompliance

--- pkg/function/ecr.go
+++ pkg/function/ecr.go
@@ -7,14 +7,15 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecr"
-	"github.com/aws/aws-sdk-go/service/ecr/ecriface"
+	"golang.org/x/sync/errgroup"
 )
 
-// RepositoryCompliant checks if the repository for the image
+// CheckRepositoryCompliance checks if the repository for the image
 // that was just sent to the is compliant.
-// 1. Has image tagging enabled
-// 2. Has image scanning enabled
-func RepositoryCompliant(ctx context.Context, repository string, svc ecriface.ECRAPI) (bool, error) {
+// 1. Comes from ECR
+// 2. Has image tagging enabled
+// 3. Has image scanning enabled
+func (c *Container) CheckRepositoryCompliance(ctx context.Context, repository string) (bool, error) {
 	input := &ecr.DescribeRepositoriesInput{
 		MaxResults:      aws.Int64(1),
 		RepositoryNames: []*string{aws.String(repository)},
@@ -22,18 +23,46 @@ func RepositoryCompliant(ctx context.Context, repository string, svc ecriface.EC
 	if err := input.Validate(); err != nil {
 		return false, err
 	}
-	output, err := svc.DescribeRepositoriesWithContext(ctx, input)
+	output, err := c.ECR.DescribeRepositoriesWithContext(ctx, input)
 	if err != nil {
 		return false, err
 	}
 	if len(output.Repositories) == 0 {
-		log.Errorf("No repositories named '%s' found:", repository)
+		log.Errorf("No repositories named '%s' found", repository)
 		return false, nil
 	}
 	r := output.Repositories[0]
 	if aws.StringValue(r.ImageTagMutability) == ecr.ImageTagMutabilityMutable || !aws.BoolValue(r.ImageScanningConfiguration.ScanOnPush) {
-		log.Errorf("Error: repository '%s' is not compliant:", repository)
+		log.Errorf("Repository '%s' is not compliant", repository)
 		return false, nil
+	}
+	return true, nil
+}
+
+// BatchCheckRepositoryCompliance checks the compliance of a given set of ECR repositories.
+// False is returned if a single repository is not compliant.
+func (c *Container) BatchCheckRepositoryCompliance(ctx context.Context, repos []string) (bool, error) {
+	g, ctx := errgroup.WithContext(ctx)
+	compliances := make([]bool, len(repos))
+
+	for i, repo := range repos {
+		i, repo := i, repo
+		g.Go(func() error {
+			compliant, err := c.CheckRepositoryCompliance(ctx, repo)
+			if err == nil {
+				compliances[i] = compliant
+			}
+			return err
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return false, err
+	}
+
+	for _, compliance := range compliances {
+		if !compliance {
+			return false, nil
+		}
 	}
 	return true, nil
 }
