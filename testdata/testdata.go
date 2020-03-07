c15cbffc050acded3d109743ccf6e9e8cc9076f1
Simon Woldemichael <swoldemichael@acm.org>
2020-03-06 23:49:37 -0600

    Update ReviewWithOneImage

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -9,7 +9,7 @@ const ReviewWithOneImage = `{
 	"kind": "AdmissionReview",
 	"apiVersion": "admission.k8s.io/v1beta1",
 	"request": {
-		"uid": "c4e1ae3b-7a3c-496a-99cb-a1f950e32edf",
+		"uid": "e77141b6-6033-11ea-8d6a-0ac25c990f4a",
 		"kind": {
 			"group": "",
 			"version": "v1",
@@ -20,45 +20,62 @@ const ReviewWithOneImage = `{
 			"version": "v1",
 			"resource": "pods"
 		},
-		"requestKind": {
-			"group": "",
-			"version": "v1",
-			"kind": "Pod"
-		},
-		"requestResource": {
-			"group": "",
-			"version": "v1",
-			"resource": "pods"
-		},
-		"namespace": "default",
+		"namespace": "echo-namespace",
 		"operation": "CREATE",
 		"userInfo": {
-			"username": "kubernetes-admin",
-			"groups": [
-				"system:masters",
-				"system:authenticated"
-			]
+			"username": "system:serviceaccount:kube-system:replicaset-controller",
+			"uid": "064184fd-5e6c-11ea-8d6a-0ac25c990f4a",
+			"groups": ["system:serviceaccounts", "system:serviceaccounts:kube-system", "system:authenticated"]
 		},
 		"object": {
 			"kind": "Pod",
 			"apiVersion": "v1",
 			"metadata": {
-				"name": "test-ecr-containers",
-				"namespace": "default",
-				"creationTimestamp": null,
+				"name": "echo-68f4474876-dzsjm",
+				"generateName": "echo-68f4474876-",
+				"namespace": "echo-namespace",
+				"uid": "e77136cf-6033-11ea-8d6a-0ac25c990f4a",
+				"creationTimestamp": "2020-03-07T05:24:23Z",
 				"labels": {
-					"cloud": "aws"
-				}
+					"app": "echo",
+					"pod-template-hash": "68f4474876"
+				},
+				"annotations": {
+					"kubernetes.io/psp": "eks.privileged"
+				},
+				"ownerReferences": [{
+					"apiVersion": "apps/v1",
+					"kind": "ReplicaSet",
+					"name": "echo-68f4474876",
+					"uid": "21494a34-6033-11ea-8d6a-0ac25c990f4a",
+					"controller": true,
+					"blockOwnerDeletion": true
+				}]
 			},
 			"spec": {
-				"containers": [{
-						"name": "generic",
-						"image": "%s",
-						"terminationMessagePath": "/dev/termination-log",
-						"terminationMessagePolicy": "FallbackToLogsOnError",
-						"imagePullPolicy": "IfNotPresent"
+				"volumes": [{
+					"name": "default-token-qrv2v",
+					"secret": {
+						"secretName": "default-token-qrv2v"
 					}
-				],
+				}],
+				"containers": [{
+					"name": "echo",
+					"image": "%s",
+					"ports": [{
+						"containerPort": 80,
+						"protocol": "TCP"
+					}],
+					"resources": {},
+					"volumeMounts": [{
+						"name": "default-token-qrv2v",
+						"readOnly": true,
+						"mountPath": "/var/run/secrets/kubernetes.io/serviceaccount"
+					}],
+					"terminationMessagePath": "/dev/termination-log",
+					"terminationMessagePolicy": "File",
+					"imagePullPolicy": "IfNotPresent"
+				}],
 				"restartPolicy": "Always",
 				"terminationGracePeriodSeconds": 30,
 				"dnsPolicy": "ClusterFirst",
@@ -66,16 +83,26 @@ const ReviewWithOneImage = `{
 				"serviceAccount": "default",
 				"securityContext": {},
 				"schedulerName": "default-scheduler",
+				"tolerations": [{
+					"key": "node.kubernetes.io/not-ready",
+					"operator": "Exists",
+					"effect": "NoExecute",
+					"tolerationSeconds": 300
+				}, {
+					"key": "node.kubernetes.io/unreachable",
+					"operator": "Exists",
+					"effect": "NoExecute",
+					"tolerationSeconds": 300
+				}],
 				"priority": 0,
 				"enableServiceLinks": true
 			},
-			"status": {}
+			"status": {
+				"phase": "Pending",
+				"qosClass": "BestEffort"
+			}
 		},
 		"oldObject": null,
-		"dryRun": false,
-		"options": {
-			"kind": "CreateOptions",
-			"apiVersion": "meta.k8s.io/v1"
-		}
+		"dryRun": false
 	}
 }`
