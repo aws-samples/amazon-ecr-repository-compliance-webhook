d9f637e0a0ca691e48481bd07742a73e7531e256
Simon Woldemichael <swoldemichael@acm.org>
2020-05-04 14:04:54 -0500

    Add license headers

--- pkg/function/container.go
+++ pkg/function/container.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package function contains library units for the amazon-ecr-repository-compliance-webhook Lambda function.
 package function
 
--- testdata/testdata.go
+++ testdata/testdata.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package testdata contains data for testing.
 package testdata
 
